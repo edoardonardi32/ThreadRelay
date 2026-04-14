17f4ff50f57c3083f34db0a5d36f16466d979413
edoardonardi <nardi.edoardo@franchettisalviani.ent>
2026-04-14 10:33:49 +0200

    Primo ocommit

--- NardiStaffettaThread/src/nardistaffettathread/NardiStaffettaThread.java
+++ NardiStaffettaThread/src/nardistaffettathread/NardiStaffettaThread.java
@@ -2,19 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package threadrelay;
 
 /**
  *
- * @author delfo
+ * @author nardi.edoardo
  */
-public class ThreadRelay {
+public class NardiStaffettaThread {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        // TODO code application logic here
     }
     
 }
